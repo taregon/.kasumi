6827fe98cdc1a16d5d7c822acb0515277670b013
taregon <taregon@proton.me>
2024-09-05 02:35:19 -0500

    No pierde el foco neotree
    Al abrir el panel, no salta al buffer

--- dotfiles/config/nvim/lua/kasumi/plugins/neo-tree.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/neo-tree.lua
@@ -1,4 +1,5 @@
 require("neo-tree").setup({
+	popup_border_style = "single",
 	source_selector = {
 		winbar = false,
 		statusline = false,
@@ -6,6 +7,23 @@ require("neo-tree").setup({
 	filesystem = {
 		follow_current_file = {
 			enabled = true,
+		},
+	},
+	event_handlers = {
+		{
+			-- Mantener el foco en el panel de Neo-tree después de abrir un archivo
+			event = "file_opened",
+			handler = function()
+				vim.cmd("wincmd p")
+			end,
+		},
+	},
+	default_component_configs = {
+		git_status = {
+			symbols = {
+				untracked = "󰘥",
+				unstaged = "󰦘", -- "󰿦"
+			},
 		},
 	},
 })
