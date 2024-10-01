26741e38a63244633308aa3a8387f383c70e2c1c
taregon <taregon@proton.me>
2024-09-30 21:37:49 -0500

    Ahora colorea en la barra
    Colores informacion:
    - extension
    - formato de texto
    - UTF-8

--- dotfiles/config/nvim/lua/kasumi/plugins/lualine.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/lualine.lua
@@ -1,7 +1,6 @@
 -- ===================================================
 -- Ajustes de NAVIC
 -- ===================================================
---
 local navic = require("nvim-navic")
 
 require("lspconfig").clangd.setup({
@@ -21,13 +20,34 @@ local function gitgutter_diff()
 end
 
 -- ===================================================
+-- CATPPUCCIN
+-- ===================================================
+local catppuccin = require("catppuccin.palettes").get_palette()
+local u = require("catppuccin.utils.colors")
+
+-- Función global para obtener el color basado en el modo actual de nvim
+local function get_mode_color()
+	local mode_color = {
+		n = u.darken(catppuccin.blue, 0.5, catppuccin.base), -- Normal mode
+		i = u.darken(catppuccin.green, 0.5, catppuccin.base), -- Insert mode
+		v = u.darken(catppuccin.mauve, 0.5, catppuccin.base), -- Visual mode
+		V = u.darken(catppuccin.mauve, 0.5, catppuccin.base), -- Visual line mode
+		c = u.darken(catppuccin.peach, 0.5, catppuccin.base), -- Command mode
+		R = u.darken(catppuccin.red, 0.5, catppuccin.base), -- Replace mode
+		[""] = u.darken(catppuccin.pink, 0.5, catppuccin.base), -- Visual block mode
+	}
+	return { fg = mode_color[vim.fn.mode()], gui = "bold" }
+end
+
+-- ===================================================
 -- LUALINE
 -- ===================================================
---
 require("lualine").setup({
 	options = {
+		-- theme = "catppuccin",
 		component_separators = { left = "›", right = "" },
 		section_separators = { left = "", right = "" },
+		globalstatus = true, -- Una barra para cuando hay mas ventanas
 	},
 	disabled_filetypes = {
 		statusline = {
@@ -68,13 +88,37 @@ require("lualine").setup({
 		lualine_c = {
 			{
 				"filename",
-				path = 1, -- ruta relativa
+				path = 4, -- Carpeta principal y nombre
 				shorting_target = 24,
 				symbols = {
-					modified = "󰧢",
-					readonly = "󰦝",
+					modified = "",
+					readonly = "󰍁",
 					newfile = "󰽃",
 				},
+			},
+		},
+		lualine_x = {
+			{
+				"fileformat",
+				symbols = {
+					unix = "󰌽",
+					dos = "󰍲",
+					mac = "",
+				},
+				color = get_mode_color,
+			},
+			{
+				"encoding",
+				fmt = string.upper,
+				color = get_mode_color,
+			},
+			{
+				"filetype",
+				-- Colorea el texto según el icono
+				color = function()
+					local _, color = require("nvim-web-devicons").get_icon_cterm_color_by_filetype(vim.bo.filetype)
+					return { fg = color }
+				end,
 			},
 		},
 	},
