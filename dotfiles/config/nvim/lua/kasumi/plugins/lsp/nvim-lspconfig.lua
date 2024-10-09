b4105bc6430d211965d847a1585b2c5727cfe8e1
taregon <taregon@proton.me>
2024-10-08 23:45:18 -0500

    Incorporacion de cmp_nvim_lsp
    Ya lo tenia, solo no lo inclui en los demas lsp

--- dotfiles/config/nvim/lua/kasumi/plugins/lsp/nvim-lspconfig.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/lsp/nvim-lspconfig.lua
@@ -1,16 +1,35 @@
 -- Configura y gestiona servidores de lenguaje (LSPs) en Neovim
 -- Previamente deben estar instalados por medio de mason-lspconfig.lua
 local lspconfig = require("lspconfig")
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-lspconfig.bashls.setup({})
-lspconfig.dockerls.setup({})
-lspconfig.graphql.setup({})
-lspconfig.html.setup({})
-lspconfig.jsonls.setup({})
-lspconfig.pyright.setup({})
+lspconfig.bashls.setup({
+	capabilities = lsp_capabilities,
+})
+
+lspconfig.dockerls.setup({
+	capabilities = lsp_capabilities,
+})
+
+lspconfig.graphql.setup({
+	capabilities = lsp_capabilities,
+})
+
+lspconfig.html.setup({
+	capabilities = lsp_capabilities,
+})
+
+lspconfig.jsonls.setup({
+	capabilities = lsp_capabilities,
+})
+
+lspconfig.pyright.setup({
+	capabilities = lsp_capabilities,
+})
 
 -- Configuración para YAML (YAML Language Server)
 lspconfig.yamlls.setup({
+	capabilities = lsp_capabilities,
 	settings = {
 		yaml = {
 			schemas = {
@@ -22,8 +41,12 @@ lspconfig.yamlls.setup({
 
 -- Configuración para Lua (sumneko_lua)
 lspconfig.lua_ls.setup({
+	capabilities = lsp_capabilities,
 	settings = {
 		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
 			diagnostics = {
 				globals = { "vim" }, -- Reconocer 'vim' como global
 			},
