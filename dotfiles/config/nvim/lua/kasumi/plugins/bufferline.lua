5267ccac0565d49db0383182c3b60068a5bb11c9
taregon <taregon@proton.me>
2024-10-20 00:29:08 -0500

    Ajustes de colores en bufferline

--- dotfiles/config/nvim/lua/kasumi/plugins/bufferline.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/bufferline.lua
@@ -5,28 +5,52 @@ local macchiato = require("catppuccin.palettes").get_palette("macchiato")
 
 require("bufferline").setup({
 	highlights = require("catppuccin.groups.integrations.bufferline").get({
-		styles = { "italic" },
+		-- styles = { "italic" },
 		custom = {
 			macchiato = {
-				fill = { bg = macchiato.base }, -- Cambia el fondo de las pestañas.
-
 				buffer_selected = {
 					bg = macchiato.surface0,
 					bold = true,
 				},
-				close_button_selected = { bg = macchiato.surface0 },
-				modified_selected = { bg = macchiato.surface0 },
-				separator = { fg = macchiato.base },
-				separator_selected = { bg = macchiato.surface0, fg = macchiato.base },
-				separator_visible = { fg = macchiato.base },
+				close_button_selected = {
+					bg = macchiato.surface0,
+				}, -- Oculta el botón de cerrar
+				fill = {
+					bg = macchiato.base,
+				},
+				modified_selected = {
+					-- fg = macchiato.yellow,
+					bg = macchiato.surface0,
+				},
+				separator = {
+					fg = macchiato.base,
+				},
+				separator_selected = {
+					bg = macchiato.surface0,
+					fg = macchiato.base,
+				},
+				separator_visible = {
+					fg = macchiato.base,
+				},
+				trunc_marker = {
+					bg = macchiato.base,
+					fg = macchiato.text,
+				}, -- Cuando hay demasiados archivos en el bufferline
 			},
 		},
 	}),
 
 	options = {
 		separator_style = "slope",
+		modified_icon = "󰗐",
 		show_close_icon = false,
 		show_buffer_close_icons = false,
-		color_icons = false, -- Mono cromático
+		color_icons = false, -- iconos mono cromáticos
+		offsets = {
+			{
+				filetype = "neo-tree",
+				text = "󰥩  File Explorer",
+			},
+		},
 	},
 })
