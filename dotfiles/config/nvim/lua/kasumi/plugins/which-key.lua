e65261eeba69b89f1bc299da8d9f06f04a967b3e
taregon <taregon@proton.me>
2024-10-26 11:45:28 -0500

    Probando atajos para comment-box

--- dotfiles/config/nvim/lua/kasumi/plugins/which-key.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/which-key.lua
@@ -1,6 +1,22 @@
 require("which-key").setup({
+	preset = "modern",
 	icons = {
 		separator = "",
+		breadcrumb = "",
 	},
 })
 require("which-key.health").check()
+
+require("which-key").add({
+	-- ──────────────────────< COMMENT BOX >───────────────────────
+	-- Puedes ver mas estilos ejecutando :CBcatalog
+	-- Repo: https://github.com/LudoPinelli/comment-box.nvim
+	{ "<leader>c", group = "󱜾  Boxes", mode = { "n", "v" } },
+	-- ────────────────────────────────────────────────────────────
+	{ "<leader>ca", ":CBlabox20<CR>", desc = "Title Squared (adaptive)" },
+	{ "<leader>cc", ":CBlcline8<CR>", desc = "Title Spiked (center)" },
+	{ "<leader>cs", ":CBllline6<CR>", desc = "Title Squared (left)" },
+	{ "<leader>cb", ":CBlcbox8<CR>", desc = "Box" },
+	{ "<leader>cl", ":CBline<CR>", desc = "Line" },
+	{ "<leader>cr", ":CBd<CR>", desc = "Remove", mode = "v" },
+})
