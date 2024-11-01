7c7190286d284c3d8ea1b2e3236e6f2af3971945
taregon <taregon@proton.me>
2024-11-01 01:20:00 -0500

    Ajutes para supermaven

--- dotfiles/config/nvim/lua/kasumi/plugins/lsp/nvim-supermaven.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/lsp/nvim-supermaven.lua
@@ -1,5 +1,9 @@
 require("supermaven-nvim").setup({
-	-- disable_inline_completion = true,
-	disable_keymaps = true,
+	disable_inline_completion = true,
+	-- disable_keymaps = true,
 	-- color = "#acb5c1",
+	disable_filetype = {
+		"txt",
+		"markdown",
+	},
 })
