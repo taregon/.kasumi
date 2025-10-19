566c6560a6042e8929a2046134d60625595c5c0a
taregon <taregon@proton.me>
2025-10-18 22:11:26 -0500

    Mejoras en las variables
    Nuevo metodo de insertar lineas

--- postinstall/01-limine.sh
+++ postinstall/01-limine.sh
@@ -5,7 +5,8 @@
 set -e # Detenerse ante cualquier error
 
 LIMINE_CONF="/boot/EFI/limine/limine.conf"
-LIMINE_BACKUP="${LIMINE_CONF}.bak"
+LIMINE_BACK="${LIMINE_CONF}.bak"
+LIMINE_TEMP="${LIMINE_CONF}.tmp"
 WALLPAPER_DST="/boot/EFI/limine/wallpaper.jpg"
 WALLPAPER_SRC=~/.kasumi/wallpaper/wall_134239802.jpg
 
@@ -21,11 +22,12 @@ if [ ! -f "$WALLPAPER_SRC" ]; then
 fi
 
 # ─[ Mensajes ]───────────────────────────────────────────────
-echo "  Creando respaldo en: $LIMINE_BACKUP"
-sudo cp "$LIMINE_CONF" "$LIMINE_BACKUP"
+echo "  Creando respaldo en: $LIMINE_BACK"
+sudo cp "$LIMINE_CONF" "$LIMINE_BACK"
 
 echo "  Insertando ajustes en limine..."
-sudo sh -c "printf '# Fondo\nwallpaper: boot():/EFI/limine/wallpaper.jpg\nwallpaper_style: centered\n\n' | cat - '$LIMINE_CONF' > '${LIMINE_CONF}.tmp' && mv '${LIMINE_CONF}.tmp' '$LIMINE_CONF'"
+printf '# Fondo\nwallpaper: boot():/EFI/limine/wallpaper.jpg\nwallpaper_style: centered\n\n' \
+    | cat - "$LIMINE_CONF" | sudo tee "$LIMINE_TEMP" > /dev/null && sudo mv "$LIMINE_TEMP" "$LIMINE_CONF"
 
 echo "  Copiando wallpaper a: $WALLPAPER_DST"
 sudo cp "$WALLPAPER_SRC" "$WALLPAPER_DST"
