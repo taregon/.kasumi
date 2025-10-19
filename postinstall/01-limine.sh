42bcf327cff8b7d5279583e9e0089697e2fad030
taregon <taregon@proton.me>
2025-10-18 23:34:28 -0500

    Empleo de EOF
    para insertar las lineas al conf

--- postinstall/01-limine.sh
+++ postinstall/01-limine.sh
@@ -2,13 +2,22 @@
 # Agrega configuración de fondo a /boot/EFI/limine/limine.conf
 # sin borrar el contenido existente y creando un respaldo previo.
 
-set -e # Detenerse ante cualquier error
+set -euo pipefail # Detenerse ante cualquier error y definir variables antes de usarlas
 
 LIMINE_CONF="/boot/EFI/limine/limine.conf"
 LIMINE_BACK="${LIMINE_CONF}.bak"
-LIMINE_TEMP="${LIMINE_CONF}.tmp"
 WALLPAPER_DST="/boot/EFI/limine/wallpaper.jpg"
 WALLPAPER_SRC=~/.kasumi/wallpaper/wall_134239802.jpg
+
+# ─[ Ajustes para el wallpaper ]──────────────────────────────
+WALL_CONF=$(
+    cat << 'EOF'
+# LIMINE WALLPAPER
+wallpaper: boot():/EFI/limine/wallpaper.jpg
+wallpaper_style: centered
+# ────────────────────────────────────────────────────────────
+EOF
+)
 
 # ─[ Verificación ]───────────────────────────────────────────
 if [ ! -f "$LIMINE_CONF" ]; then
@@ -22,14 +31,27 @@ if [ ! -f "$WALLPAPER_SRC" ]; then
 fi
 
 # ─[ Mensajes ]───────────────────────────────────────────────
+echo "Agregando configuración del fondo de pantalla en Limine,"
 echo "  Creando respaldo en: $LIMINE_BACK"
 sudo cp "$LIMINE_CONF" "$LIMINE_BACK"
 
-echo "  Insertando ajustes en limine..."
-printf '# Fondo\nwallpaper: boot():/EFI/limine/wallpaper.jpg\nwallpaper_style: centered\n\n' \
-    | cat - "$LIMINE_CONF" | sudo tee "$LIMINE_TEMP" > /dev/null && sudo mv "$LIMINE_TEMP" "$LIMINE_CONF"
+echo "  Insertando ajustes..."
 
-echo "  Copiando wallpaper a: $WALLPAPER_DST"
+# Comprueba si el bloque ya existe
+if ! grep -Fq "$WALL_CONF" "$LIMINE_CONF"; then
+    # Insertar al inicio
+    {
+        printf "%s\n" "$WALL_CONF"
+        cat "$LIMINE_CONF"
+    } | sudo tee "${LIMINE_CONF}.tmp" > /dev/null && sudo mv "${LIMINE_CONF}.tmp" "$LIMINE_CONF" || {
+        echo "  Error al modificar el archivo"
+        exit 1
+    }
+else
+    echo "  Bloque ya insertado, no se hace nada"
+fi
+
+echo "  Copiando wallpaper a: $WALLPAPER_DST"
 sudo cp "$WALLPAPER_SRC" "$WALLPAPER_DST"
 
 echo
