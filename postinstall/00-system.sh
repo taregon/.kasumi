fbf7fc1492e1b95850cedd7f6d97c99d08d3143f
taregon <taregon@proton.me>
2025-10-21 17:43:44 -0500

    Ordenando funciones
    Espero que sigan funcionando

--- postinstall/00-system.sh
+++ postinstall/00-system.sh
@@ -70,6 +70,93 @@ prepare_system() {
 # ╒════════════════════════════════════════════════════════════╕
 # │                    FUNCIONES POR ÁREAS                     │
 # ╘════════════════════════════════════════════════════════════╛
+install_app_luakit() {
+    echo ">> Instalando Luakit y complementos multimedia"
+    local pkgs=(
+        luakit           # Navegador web minimalista basado en WebKit
+        gst-plugins-good # Plugins GStreamer recomendados
+        gst-plugins-bad  # Plugins GStreamer menos comunes, algunos experimentales
+        gst-plugins-ugly # Plugins GStreamer con licencias restrictivas
+        gst-libav        # Soporte para códecs FFmpeg en GStreamer
+        hunspell         # Corrector ortográfico
+        hunspell-es_pa   # Diccionario español (Panamá) para Hunspell
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_app_ranger() {
+    echo ">> Instalando ranger y complementos"
+    local pkgs=(
+        atool          # Soporte para archivos comprimidos
+        imagemagick    # Conversión y vista previa de imágenes
+        mediainfo      # Muestra metadatos de audio y video
+        poppler        # Vista previa y utilidades para PDF
+        python-chardet # Detección de codificación de texto
+        python-pillow  # Librería para procesar imágenes (PIL)
+        ranger         # Gestor de archivos en terminal tipo VI
+        trash-cli      # Envío de archivos a la papelera desde CLI
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_app_zathura() {
+    echo ">> Instalando visor PDF y complementos OCR"
+    local pkgs=(
+        zathura            # Visor de documentos ligero
+        zathura-pdf-mupdf  # Backend para abrir archivos PDF en Zathura
+        tesseract-data-eng # Datos de reconocimiento OCR en inglés
+        tesseract-data-spa # Datos de reconocimiento OCR en español
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_compression_tools() {
+    echo ">> Instalando herramientas de compresión más comunes"
+    local pkgs=(
+        engrampa # GUI MATE para archivos comprimidos
+        p7zip    # .7z
+        unrar    # .rar
+        unzip    # Descomprimir .zip
+        zip      # Comprimir archivos .zip
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_editors() {
+    echo ">> Instalando editores y herramientas de desarrollo"
+    local pkgs=(
+        # Editores
+        obsidian               # Editor de notas (AUR)
+        visual-studio-code-bin # IDE completo (AUR)
+        xed                    # Editor de texto simple (MATE)
+
+        # NEOVIM y dependencias
+        luarocks # Gestor de paquetes para Lua (plugins de nvim)
+        neovim   # Editor de texto/IDE ligero
+        nodejs   # Requerido por VSCode y plugins de nvim
+        npm      # Gestor de paquetes para Node.js
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_file_management() {
+    echo ">> Instalando herramientas de archivos y multimedia"
+    local pkgs=(
+        exfat-utils   # Sistemas de archivos exFAT
+        gvfs          # Sistema virtual de archivos
+        gvfs-gphoto2  # Acceso cámaras PTP
+        gvfs-mtp      # Acceso MTP
+        imv           # Visor de imágenes ligero
+        mpv           # Reproductor multimedia
+        mtpfs         # Montaje de MTP
+        ntfs-3g       # Soporte NTFS
+        thunar        # Explorador de archivos GTK
+        thunar-volman # Gestión de volúmenes para Thunar
+        tumbler       # Generador de miniaturas
+        udisks2       # Gestión de discos y volúmenes
+    )
+    instalar "${pkgs[@]}"
+}
 
 install_fonts() {
     echo ">> Instalando fuentes y tipografías"
@@ -95,28 +182,21 @@ install_fonts() {
     instalar "${pkgs[@]}"
 }
 
-install_ranger() {
-    echo ">> Instalando ranger y complementos"
+install_man() {
+    echo ">> Instalando documentación y manuales"
     local pkgs=(
-        atool          # Soporte para archivos comprimidos
-        imagemagick    # Conversión y vista previa de imágenes
-        mediainfo      # Muestra metadatos de audio y video
-        poppler        # Vista previa y utilidades para PDF
-        python-chardet # Detección de codificación de texto
-        python-pillow  # Librería para procesar imágenes (PIL)
-        ranger         # Gestor de archivos en terminal tipo VI
-        trash-cli      # Envío de archivos a la papelera desde CLI
+        man          # Comando man
+        man-db       # Base de datos de man
+        man-pages-es # Páginas de manual en español
     )
     instalar "${pkgs[@]}"
 }
 
-install_zathura() {
-    echo ">> Instalando visor PDF y complementos OCR"
+install_misc_tools() {
+    echo ">> Instalando herramientas varias"
     local pkgs=(
-        zathura            # Visor de documentos ligero
-        zathura-pdf-mupdf  # Backend para abrir archivos PDF en Zathura
-        tesseract-data-eng # Datos de reconocimiento OCR en inglés
-        tesseract-data-spa # Datos de reconocimiento OCR en español
+        localsend-bin # Compartir archivos en red local
+        spek-x-git    # Analizador de espectro de audio
     )
     instalar "${pkgs[@]}"
 }
@@ -156,54 +236,6 @@ install_system_tools() {
     instalar "${pkgs[@]}"
 }
 
-install_misc_tools() {
-    echo ">> Instalando herramientas varias"
-    local pkgs=(
-        localsend-bin # Compartir archivos en red local
-        spek-x-git    # Analizador de espectro de audio
-    )
-    instalar "${pkgs[@]}"
-}
-
-install_compression_tools() {
-    echo ">> Instalando herramientas de compresión más comunes"
-    local pkgs=(
-        engrampa # GUI MATE para archivos comprimidos
-        p7zip    # .7z
-        unrar    # .rar
-        unzip    # Descomprimir .zip
-        zip      # Comprimir archivos .zip
-    )
-    instalar "${pkgs[@]}"
-}
-
-install_editors() {
-    echo ">> Instalando editores y herramientas de desarrollo"
-    local pkgs=(
-        # Editores
-        obsidian               # Editor de notas (AUR)
-        visual-studio-code-bin # IDE completo (AUR)
-        xed                    # Editor de texto simple (MATE)
-
-        # NEOVIM y dependencias
-        luarocks # Gestor de paquetes para Lua (plugins de nvim)
-        neovim   # Editor de texto/IDE ligero
-        nodejs   # Requerido por VSCode y plugins de nvim
-        npm      # Gestor de paquetes para Node.js
-    )
-    instalar "${pkgs[@]}"
-}
-
-install_man() {
-    echo ">> Instalando documentación y manuales"
-    local pkgs=(
-        man          # Comando man
-        man-db       # Base de datos de man
-        man-pages-es # Páginas de manual en español
-    )
-    instalar "${pkgs[@]}"
-}
-
 install_terminal_utils() {
     echo ">> Instalando utilidades de terminal"
     local pkgs=(
@@ -233,39 +265,6 @@ install_terminal_utils() {
     echo "[!] Reinicia la sesión para aplicar el cambio de shell"
 }
 
-install_file_management() {
-    echo ">> Instalando herramientas de archivos y multimedia"
-    local pkgs=(
-        exfat-utils   # Sistemas de archivos exFAT
-        gvfs          # Sistema virtual de archivos
-        gvfs-gphoto2  # Acceso cámaras PTP
-        gvfs-mtp      # Acceso MTP
-        imv           # Visor de imágenes ligero
-        mpv           # Reproductor multimedia
-        mtpfs         # Montaje de MTP
-        ntfs-3g       # Soporte NTFS
-        thunar        # Explorador de archivos GTK
-        thunar-volman # Gestión de volúmenes para Thunar
-        tumbler       # Generador de miniaturas
-        udisks2       # Gestión de discos y volúmenes
-    )
-    instalar "${pkgs[@]}"
-}
-
-install_luakit() {
-    echo ">> Instalando Luakit y complementos multimedia"
-    local pkgs=(
-        luakit           # Navegador web minimalista basado en WebKit
-        gst-plugins-good # Plugins GStreamer recomendados
-        gst-plugins-bad  # Plugins GStreamer menos comunes, algunos experimentales
-        gst-plugins-ugly # Plugins GStreamer con licencias restrictivas
-        gst-libav        # Soporte para códecs FFmpeg en GStreamer
-        hunspell         # Corrector ortográfico
-        hunspell-es_pa   # Diccionario español (Panamá) para Hunspell
-    )
-    instalar "${pkgs[@]}"
-}
-
 install_theme() {
     echo ">> Instalando temas y elementos de estética"
     local pkgs=(
@@ -288,7 +287,7 @@ mostrar_menu() {
     echo
     echo -e "\e[36m = = = Instalador de Paquetes (con paru) = = =\e[0m"
     # Opciones en amarillo + texto normal
-    echo -e "\e[33m1)\e[0m Preparar sistema (Obligatorio)"
+    echo -e "\e[33m1)\e[0m Preparar sistema (\e[31mObligatorio\e[0m)"
     echo -e "\e[33m2)\e[0m Utilidades del sistema y entorno gráfico"
     echo -e "\e[33m3)\e[0m Editores y herramientas de desarrollo"
     echo -e "\e[33m4)\e[0m Administración de archivos y multimedia"
