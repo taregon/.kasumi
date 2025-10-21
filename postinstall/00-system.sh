5043f476beebd3ba8b1d4c61e948dfda88090d8d
taregon <taregon@proton.me>
2025-10-21 17:57:10 -0500

    Reordenando codigo

--- postinstall/00-system.sh
+++ postinstall/00-system.sh
@@ -84,6 +84,27 @@ install_app_luakit() {
     instalar "${pkgs[@]}"
 }
 
+install_app_man() {
+    echo ">> Instalando documentación y manuales"
+    local pkgs=(
+        man          # Comando man
+        man-db       # Base de datos de man
+        man-pages-es # Páginas de manual en español
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_app_neovim() {
+    echo ">> Instalando editor nvim"
+    local pkgs=(
+        luarocks # Gestor de paquetes para Lua (plugins de nvim)
+        neovim   # Editor de texto/IDE ligero
+        nodejs   # Requerido por VSCode y plugins de nvim
+        npm      # Gestor de paquetes para Node.js
+    )
+    instalar "${pkgs[@]}"
+}
+
 install_app_ranger() {
     echo ">> Instalando ranger y complementos"
     local pkgs=(
@@ -110,55 +131,21 @@ install_app_zathura() {
     instalar "${pkgs[@]}"
 }
 
-install_compression_tools() {
-    echo ">> Instalando herramientas de compresión más comunes"
+install_misc_tools() {
+    echo ">> Instalando herramientas varias"
     local pkgs=(
-        engrampa # GUI MATE para archivos comprimidos
-        p7zip    # .7z
-        unrar    # .rar
-        unzip    # Descomprimir .zip
-        zip      # Comprimir archivos .zip
-    )
-    instalar "${pkgs[@]}"
-}
+        localsend-bin # Compartir archivos en red local
+        spek-x-git    # Analizador de espectro de audio
 
-install_editors() {
-    echo ">> Instalando editores y herramientas de desarrollo"
-    local pkgs=(
         # Editores
         obsidian               # Editor de notas (AUR)
         visual-studio-code-bin # IDE completo (AUR)
         xed                    # Editor de texto simple (MATE)
-
-        # NEOVIM y dependencias
-        luarocks # Gestor de paquetes para Lua (plugins de nvim)
-        neovim   # Editor de texto/IDE ligero
-        nodejs   # Requerido por VSCode y plugins de nvim
-        npm      # Gestor de paquetes para Node.js
     )
     instalar "${pkgs[@]}"
 }
 
-install_file_management() {
-    echo ">> Instalando herramientas de archivos y multimedia"
-    local pkgs=(
-        exfat-utils   # Sistemas de archivos exFAT
-        gvfs          # Sistema virtual de archivos
-        gvfs-gphoto2  # Acceso cámaras PTP
-        gvfs-mtp      # Acceso MTP
-        imv           # Visor de imágenes ligero
-        mpv           # Reproductor multimedia
-        mtpfs         # Montaje de MTP
-        ntfs-3g       # Soporte NTFS
-        thunar        # Explorador de archivos GTK
-        thunar-volman # Gestión de volúmenes para Thunar
-        tumbler       # Generador de miniaturas
-        udisks2       # Gestión de discos y volúmenes
-    )
-    instalar "${pkgs[@]}"
-}
-
-install_fonts() {
+install_sys_fonts() {
     echo ">> Instalando fuentes y tipografías"
     local pkgs=(
         noto-fonts-cjk
@@ -182,26 +169,7 @@ install_fonts() {
     instalar "${pkgs[@]}"
 }
 
-install_man() {
-    echo ">> Instalando documentación y manuales"
-    local pkgs=(
-        man          # Comando man
-        man-db       # Base de datos de man
-        man-pages-es # Páginas de manual en español
-    )
-    instalar "${pkgs[@]}"
-}
-
-install_misc_tools() {
-    echo ">> Instalando herramientas varias"
-    local pkgs=(
-        localsend-bin # Compartir archivos en red local
-        spek-x-git    # Analizador de espectro de audio
-    )
-    instalar "${pkgs[@]}"
-}
-
-install_network_manager() {
+install_sys_network_manager() {
     echo ">> Instalando NetworkManager y complementos"
     local pkgs=(
         networkmanager         # Servicio principal para gestionar redes
@@ -213,7 +181,20 @@ install_network_manager() {
     sudo systemctl enable --now NetworkManager
 }
 
-install_system_tools() {
+install_sys_theme() {
+    echo ">> Instalando temas y elementos de estética"
+    local pkgs=(
+        gtk-engine-murrine # Motor de temas GTK clásico
+        gtk-engines        # Motores de temas GTK adicionales
+        nwg-look           # Gestor de temas e iconos NWG
+        matcha-gtk-theme   # Tema GTK Matcha
+        arc-gtk-theme      # Tema GTK Arc
+        zukitwo-themes-git # Temas GTK Zukitwo (AUR)
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_sys_tools() {
     echo ">> Instalando herramientas del sistema y utilidades Wayland"
     local pkgs=(
         # Monitoreo y sensores
@@ -236,7 +217,38 @@ install_system_tools() {
     instalar "${pkgs[@]}"
 }
 
-install_terminal_utils() {
+install_utils_compression() {
+    echo ">> Instalando herramientas de compresión más comunes"
+    local pkgs=(
+        engrampa # GUI MATE para archivos comprimidos
+        p7zip    # .7z
+        unrar    # .rar
+        unzip    # Descomprimir .zip
+        zip      # Comprimir archivos .zip
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_utils_file_management() {
+    echo ">> Instalando herramientas de archivos y multimedia"
+    local pkgs=(
+        exfat-utils   # Sistemas de archivos exFAT
+        gvfs          # Sistema virtual de archivos
+        gvfs-gphoto2  # Acceso cámaras PTP
+        gvfs-mtp      # Acceso MTP
+        imv           # Visor de imágenes ligero
+        mpv           # Reproductor multimedia
+        mtpfs         # Montaje de MTP
+        ntfs-3g       # Soporte NTFS
+        thunar        # Explorador de archivos GTK
+        thunar-volman # Gestión de volúmenes para Thunar
+        tumbler       # Generador de miniaturas
+        udisks2       # Gestión de discos y volúmenes
+    )
+    instalar "${pkgs[@]}"
+}
+
+install_utils_terminal() {
     echo ">> Instalando utilidades de terminal"
     local pkgs=(
         bat                # Mejor cat con resaltado de sintaxis
@@ -263,19 +275,6 @@ install_terminal_utils() {
     echo "Cambiando shell predeterminada a zsh..."
     chsh -s "$(which zsh)"
     echo "[!] Reinicia la sesión para aplicar el cambio de shell"
-}
-
-install_theme() {
-    echo ">> Instalando temas y elementos de estética"
-    local pkgs=(
-        gtk-engine-murrine # Motor de temas GTK clásico
-        gtk-engines        # Motores de temas GTK adicionales
-        nwg-look           # Gestor de temas e iconos NWG
-        matcha-gtk-theme   # Tema GTK Matcha
-        arc-gtk-theme      # Tema GTK Arc
-        zukitwo-themes-git # Temas GTK Zukitwo (AUR)
-    )
-    instalar "${pkgs[@]}"
 }
 
 # ╒════════════════════════════════════════════════════════════╕
