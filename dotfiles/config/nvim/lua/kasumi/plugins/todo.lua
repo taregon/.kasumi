b0e6f4446d69700901dae80c35db032c36da1519
taregon <taregon@proton.me>
2025-10-30 17:51:41 -0500

    Nuevos iconos

--- dotfiles/config/nvim/lua/kasumi/plugins/todo.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/todo.lua
@@ -1,7 +1,11 @@
 require("todo-comments").setup({
 	keywords = {
-		FIX = { icon = "", color = "error", alt = { "FIXME", "BUG" } },
-		TODO = { icon = "", color = "info" },
-		WARN = { icon = "", color = "warning", alt = { "advertencia", "ADVERTENCIA" } },
+		FIX = { icon = "󰶯", color = "error", alt = { "FIXME", "BUG" } },
+		NOTE = { icon = "", color = "hint", alt = { "NOTA" } },
+		TODO = { icon = "󱖫", color = "info", "pendiente" },
+		WARN = { icon = "󱇏", color = "warning", alt = { "advertencia", "ADVERTENCIA" } },
+	},
+	highlight = {
+		multiline = false, -- Si esta desactivado, solo resalta una linea.
 	},
 })
