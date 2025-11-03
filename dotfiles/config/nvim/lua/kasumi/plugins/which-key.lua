612cd2de5d1df3a1a1314e7317390c758c96c996
taregon <taregon@proton.me>
2025-11-03 13:11:49 -0500

    Erratas

--- dotfiles/config/nvim/lua/kasumi/plugins/which-key.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/which-key.lua
@@ -1,3 +1,8 @@
+-- ┌──────────────────────────────────┐
+-- │░█░█░█░█░▀█▀░█▀▀░█░█░░░█░█░█▀▀░█░█│
+-- │░█▄█░█▀█░░█░░█░░░█▀█░░░█▀▄░█▀▀░░█░│
+-- │░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░▀░░░▀░▀░▀▀▀░░▀░│
+-- └──────────────────────────────────┘
 require("which-key").setup({
 	preset = "modern",
 	icons = {
@@ -8,23 +13,21 @@ require("which-key").setup({
 require("which-key.health").check()
 
 -- ╒═══════════════════════════════════════════════════════════╕
--- │            Función para alternar virtual text             │
+-- │            FUNCIÓN PARA ALTERNAR VIRTUAL TEXT             │
 -- ╘═══════════════════════════════════════════════════════════╛
 local toggle_virtual_text = function()
 	local current = vim.diagnostic.config().virtual_text
 	vim.diagnostic.config({ virtual_text = not current })
 	if not current then
-		vim.notify("Virtual text ACTIVADO")
+		vim.notify("Virtual Text ACTIVADO")
 	else
-		vim.notify("Virtual text DESACTIVADO")
+		vim.notify("Virtual Text DESACTIVADO")
 	end
 end
 
--- ┌──────────────────────────────────┐
--- │░█░█░█░█░▀█▀░█▀▀░█░█░░░█░█░█▀▀░█░█│
--- │░█▄█░█▀█░░█░░█░░░█▀█░░░█▀▄░█▀▀░░█░│
--- │░▀░▀░▀░▀░▀▀▀░▀▀▀░▀░▀░░░▀░▀░▀▀▀░░▀░│
--- └──────────────────────────────────┘
+-- ╒═══════════════════════════════════════════════════════════╕
+-- │                          ATAJOS                           │
+-- ╘═══════════════════════════════════════════════════════════╛
 require("which-key").add({
 	-- ──────────────────────< COMMENT BOX >───────────────────────
 	-- Puedes ver mas estilos ejecutando :CBcatalog
@@ -44,12 +47,13 @@ require("which-key").add({
 	-- ────────────────────────────────────────────────────────────
 	{ "<leader>tt", toggle_virtual_text, desc = "Toggle Virtual Text" },
 	{ "<leader>tr", vim.diagnostic.open_float, desc = "Show diagnostic" },
-	{ "<leader>ti", ":set invlist!<CR>:GitGutterSignsToggle<CR>", desc = "Toggle Special Chars" },
+	{ "<leader>ti", ":set invlist!<CR>", desc = "Toggle Special Chars" },
 	{ "<leader>tn", ":set invnumber! invrelativenumber!<CR>", desc = "Toggle Line Numbers" },
+	{ "<leader>tc", "<cmd>lua require('coerce').to_title()<CR>", desc = "Title Case", mode = "v" },
 	-- ────────────────────────────────────────────────────────────
 	{ "<leader>g", group = "  Hunks", mode = { "n", "v" } },
 	-- ────────────────────────────────────────────────────────────
-	{ "<leader>gp", ":Gitsigns preview_hunk<CR>", desc = "Preview hunk" },
-	{ "<leader>gr", ":Gitsigns reset_hunk_inline<CR>", desc = "Revert hunk" },
+	{ "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", desc = "Preview hunk" },
+	{ "<leader>gr", ":Gitsigns reset_hunk<CR>", desc = "Revert hunk" },
 	{ "<leader>gs", ":Gitsigns stage_hunk<CR>", desc = "Stage hunk", mode = "v" },
 })
