e5334099bcdfe78e348365b6d545136129408689
taregon <taregon@proton.me>
2025-11-04 22:58:50 -0500

    Nueva palabra para TODO

--- dotfiles/config/nvim/lua/kasumi/plugins/todo.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/todo.lua
@@ -7,13 +7,13 @@ require("todo-comments").setup({
 	keywords = {
 		FIX = { icon = "󰶯", color = "error", alt = { "FIXME", "BUG" } },
 		NOTE = { icon = "", color = "hint", alt = { "NOTA" } },
-		TODO = { icon = "󱖫", color = "info", alt = { "pendiente", "REVISAR" } },
+		TODO = { icon = "󱖫", color = "info", alt = { "pendiente", "REVISAR", "PEND" } },
 		WARN = { icon = "󱇏", color = "warning", alt = { "advertencia", "ADVERTENCIA" } },
 	},
 	highlight = {
 		multiline = false, -- Si esta desactivado, solo resalta una linea.
 	},
 	colors = {
-		error = "#ff747e",
+		-- error = "#ff747e", -- Linea de ejemplo
 	},
 })
