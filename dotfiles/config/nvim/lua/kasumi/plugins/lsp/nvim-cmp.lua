6343c7c0e47c13847a75864e8981371ccf7c6e11
taregon <taregon@proton.me>
2025-11-10 03:21:15 -0500

    Refactor

--- dotfiles/config/nvim/lua/kasumi/plugins/lsp/nvim-cmp.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/lsp/nvim-cmp.lua
@@ -12,7 +12,7 @@ local lspkind = require("lspkind")
 --   al borde de la ventana de auto completado
 -- └────────────────────────────────────────────┘
 local colors = require("catppuccin.palettes").get_palette()
-vim.api.nvim_set_hl(0, "CmpComFloatBorder", { fg = colors.green }) -- Usa u color de Catppuccin
+vim.api.nvim_set_hl(0, "CmpComFloatBorder", { fg = colors.green }) -- Usa un color de Catppuccin
 
 -- Carga los snippets.
 -- NOTA: admite varias rutas.
@@ -39,91 +39,24 @@ cmp.setup({
 		end,
 	},
 
-	-- mapping = cmp.mapping.preset.insert({
-	-- 	["<CR>"] = cmp.mapping.confirm({ select = true }), -- Acepta la selección con Enter
-	-- 	["<tab>"] = cmp.mapping(function(original)
-	-- 		if cmp.visible() then
-	-- 			cmp.select_next_item() -- run completion selection if completing
-	-- 		elseif luasnip.expand_or_jumpable() then
-	-- 			luasnip.expand_or_jump() -- expand snippets
-	-- 		else
-	-- 			original() -- run the original behavior if not completing
-	-- 		end
-	-- 	end, { "i", "s" }),
-	-- 	["<S-tab>"] = cmp.mapping(function(original)
-	-- 		if cmp.visual() then
-	-- 			cmp.select_prev_item()
-	-- 		elseif luasnip.expand_or_jumpable() then
-	-- 			luasnip.jump(-1)
-	-- 		else
-	-- 			original()
-	-- 		end
-	-- 	end, { "i", "s" }),
-	-- }),
-	-- mapping = cmp.mapping.preset.insert({
-	-- 	["<Up>"] = cmp.mapping.select_prev_item(),
-	-- 	["<Down>"] = cmp.mapping.select_next_item(),
-	-- 	["<C-d>"] = cmp.mapping.scroll_docs(-4),
-	-- 	["<C-f>"] = cmp.mapping.scroll_docs(4),
-	-- 	["<C-Space>"] = cmp.mapping.complete(),
-	-- 	["<C-e>"] = cmp.mapping.abort(),
-	-- 	["<CR>"] = cmp.mapping.confirm({ select = true }),
-	-- }),
-	--
-	-- cmp.setup({
-	-- 	mapping = {
-	-- 		-- Navegar por el menú de autocompletado
-	-- 		["<Tab>"] = cmp.mapping(function(fallback)
-	-- 			if cmp.visible() then
-	-- 				cmp.select_next_item(select_opts) -- siguiente item
-	-- 			elseif luasnip.expand_or_jumpable() then
-	-- 				luasnip.expand_or_jump() -- expandir snippet o saltar
-	-- 			else
-	-- 				fallback() -- comportamiento normal de Tab
-	-- 			end
-	-- 		end, { "i", "s" }),
+	-- Configura los atajos de teclado predeterminados para el modo de inserción
+	mapping = cmp.mapping.preset.insert({
+		-- Navegar por el menú
+		["<Up>"] = cmp.mapping.select_prev_item(),
+		["<Down>"] = cmp.mapping.select_next_item(),
 
-	-- 		["<S-Tab>"] = cmp.mapping(function(fallback)
-	-- 			if cmp.visible() then
-	-- 				cmp.select_prev_item(select_opts) -- item anterior
-	-- 			elseif luasnip.jumpable(-1) then
-	-- 				luasnip.jump(-1) -- saltar hacia atrás en snippet
-	-- 			else
-	-- 				fallback() -- comportamiento normal de Shift+Tab
-	-- 			end
-	-- 		end, { "i", "s" }),
+		-- REVISAR: Scroll en la ventana de documentación
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-	-- 		-- Confirmar selección con Enter
-	-- 		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		-- Forzar apertura del menú
+		["<TAB>"] = cmp.mapping.complete(),
 
-	-- 		-- Scroll de documentación
-	-- 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-	-- 		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		-- Cancelar el menú
+		["<C-e>"] = cmp.mapping.abort(),
 
-	-- 		-- Cerrar menú
-	-- 		["<C-e>"] = cmp.mapping.abort(),
-	-- 	},
-	-- }),
-
-	cmp.setup({
-		mapping = cmp.mapping.preset.insert({
-			-- Navegar por el menú
-			["<Up>"] = cmp.mapping.select_prev_item(),
-			["<Down>"] = cmp.mapping.select_next_item(),
-
-			-- REVISAR: Scroll en la ventana de documentación
-			["<C-d>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-			-- Forzar apertura del menú
-			["<TAB>"] = cmp.mapping.complete(),
-
-			-- Cancelar el menú
-			["<C-e>"] = cmp.mapping.abort(),
-
-			-- Confirmar selección
-			["<CR>"] = cmp.mapping.confirm({ select = true }),
-		}),
+		-- Confirmar selección
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 	}),
 
 	-- Orígenes del auto completado
