59e3bd9869f050ac912234b3dfaade8f895d6a83
taregon <taregon@proton.me>
2025-12-19 15:43:18 -0500

    Moví los ajustes de bqf.lua

--- dotfiles/config/nvim/lua/kasumi/plugins/fzf.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/fzf.lua
@@ -3,14 +3,6 @@ require("fzf-lua").setup({
 		border = "single",
 	},
 	preview = {
-		-- layout = "vertical", -- o 'horizontal', según prefieras
-		-- vertical = "down:45%", -- tamaño relativo de la vista previa
-		border = "single",
-	},
-})
-
-require("bqf").setup({
-	preview = {
 		border = "single",
 	},
 })
