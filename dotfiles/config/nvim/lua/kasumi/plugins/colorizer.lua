fc87bd84f2bb12df528a1bef22f3935032024312
taregon <taregon@proton.me>
2026-01-02 15:12:36 -0500

    Leves ajustes

--- dotfiles/config/nvim/lua/kasumi/plugins/colorizer.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/colorizer.lua
@@ -1,2 +1,7 @@
 -- Habilita los colores HEX
-require("colorizer").setup()
+require("colorizer").setup({
+	"*",
+}, {
+	names = false, -- desactiva nombres
+	RRGGBBAA = true, -- activa hex con alfa
+})
