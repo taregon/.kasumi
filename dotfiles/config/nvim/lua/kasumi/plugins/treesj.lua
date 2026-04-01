3caba83f1580a3488d6f7a59e77cb02d0ab8004b
taregon <taregon@proton.me>
2026-04-01 01:00:31 -0500

    feat(treesj): agregar configuración básica

---
+++ dotfiles/config/nvim/lua/kasumi/plugins/treesj.lua
@@ -0,0 +1,6 @@
+-- split/join inteligente de arrays, objetos y bloques con Tree-sitter
+
+require("treesj").setup({
+	use_default_keymaps = false,
+	-- notify = false, -- evita mensajes molestos
+})
