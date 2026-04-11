222ab8f1f04c1d779d2b8e1bcf51d3972bb49fd3
taregon <taregon@proton.me>
2026-04-10 20:56:51 -0500

    feat(nvim-cmp): añade soporte de colores personalizados para iconos de completado
    - modifica `lspkind` para aplicar colores de `nvim-highlight-colors` al formato de iconos
    - elimina comentario redundante en `sources`
    - actualiza descripción de keymap para incluir "atajos" en plural
    - implementa función personalizada para combinar iconos con grupos de highlight
    - usa `lazy_load` en lugar de `load` para cargar snippets de Lua

--- dotfiles/config/nvim/lua/kasumi/plugins/lsp/nvim-cmp.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/lsp/nvim-cmp.lua
@@ -16,7 +16,7 @@ vim.api.nvim_set_hl(0, "CmpComFloatBorder", { fg = colors.green }) -- Usa un col
 
 -- Carga los snippets.
 -- NOTA: admite varias rutas.
-require("luasnip.loaders.from_lua").load({
+require("luasnip.loaders.from_lua").lazy_load({
 	paths = {
 		"~/.config/nvim/lua/kasumi/goodies/snippets/",
 	},
@@ -39,13 +39,13 @@ cmp.setup({
 		end,
 	},
 
-	-- Configura los atajos de teclado predeterminados para el modo de inserción
+	-- Configura los keymaps / atajos de teclado predeterminados para el modo de inserción
 	mapping = cmp.mapping.preset.insert({
 		-- Navegar por el menú
 		["<Up>"] = cmp.mapping.select_prev_item(),
 		["<Down>"] = cmp.mapping.select_next_item(),
 
-		-- REVISAR: Scroll en la ventana de documentación
+		-- PEND: Scroll en la ventana de documentación
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 
@@ -61,7 +61,7 @@ cmp.setup({
 
 	-- Orígenes del auto completado
 	sources = cmp.config.sources({
-		{ name = "buffer", dup = 0 }, -- Textos del buffer actual
+		-- { name = "buffer", dup = 0 }, -- Textos del buffer actual
 		{ name = "luasnip" }, -- snippets
 		{ name = "nvim_lsp" }, -- LSP
 		{ name = "path" }, -- rutas del sistema
@@ -71,14 +71,30 @@ cmp.setup({
 	-- Iconos para LSP
 	formatting = {
 		fields = { "abbr", "kind", "menu" }, -- Agrupa los iconos en el menú
-		format = lspkind.cmp_format({
-			mode = "symbol_text",
-			maxwidth = 50,
-			ellipsis_char = "󱗘 ",
-			symbol_map = {
-				Supermaven = "",
-			},
-		}),
+		format = function(entry, item)
+			-- 1. Obtener info de color
+			local color_item = require("nvim-highlight-colors").format(entry, {
+				kind = item.kind,
+			})
+
+			-- 2. Aplicar lspkind (configuración original)
+			item = require("lspkind").cmp_format({
+				mode = "symbol_text",
+				maxwidth = 50,
+				ellipsis_char = "󱗘 ",
+				symbol_map = {
+					Supermaven = "",
+				},
+			})(entry, item)
+
+			-- 3. Si hay color, sobrescribir icono + highlight
+			if color_item and color_item.abbr_hl_group then
+				item.kind_hl_group = color_item.abbr_hl_group
+				item.kind = color_item.abbr
+			end
+
+			return item
+		end,
 	},
 
 	-- Agregando bordes al menu de auto completado
