399e099678b5b5313c07a0df1cdd608c75d6f249
taregon <taregon@proton.me>
2026-04-14 01:26:52 -0500

    feat(spectre): añadir modo interactivo de búsqueda y reemplazo
    - Configurar opción `color_devicons` como opcional.
    - Agregar opciones de personalización para `highlight`:

---
+++ dotfiles/config/nvim/lua/kasumi/plugins/spectre.lua
@@ -0,0 +1,15 @@
+-- ┌────────────────────────────┐
+-- │░█▀▀░█▀█░█▀▀░█▀▀░▀█▀░█▀▄░█▀▀│
+-- │░▀▀█░█▀▀░█▀▀░█░░░░█░░█▀▄░█▀▀│
+-- │░▀▀▀░▀░░░▀▀▀░▀▀▀░░▀░░▀░▀░▀▀▀│
+-- └────────────────────────────┘
+-- Búsqueda y reemplazo interactivo en múltiples archivos
+
+require("spectre").setup({
+	color_devicons = false,
+	highlight = {
+		-- ui = "Label",
+		search = "DiffDelete",
+		replace = "DiffText",
+	},
+})
