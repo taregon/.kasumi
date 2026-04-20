5ee0165d8d2d4ccd34b41f37a4c1d53154554b83
taregon <taregon@proton.me>
2026-04-20 00:19:36 -0500

    refactor(mason-tool-installer): Reorganizar la configuración de herramientas instaladas y opciones
    - Mover las herramientas a categorías de LINTERS y FORMATTERS
    - Configurar opciones de auto_update, debounce_hours y run_on_start
    - Configurar la sección integrations para mason-lspconfig y mason-null-ls

--- dotfiles/config/nvim/lua/kasumi/plugins/lsp/mason-tool-installer.lua
+++ dotfiles/config/nvim/lua/kasumi/plugins/lsp/mason-tool-installer.lua
@@ -10,43 +10,39 @@
 
 require("mason-tool-installer").setup({
 	ensure_installed = {
-		-- ───────────────────────< Shell / Zsh >──────────────────
-		"beautysh", -- Formateador para scripts shell, pero en mi caso: ZSH
-		"shellcheck", -- Linter para shell
-		"shfmt", -- Formateador y parser para bash/sh
+		-- ─────────────────────────< LINTERS >─────────────────────────
+		"jsonlint", -- JSON
+		"luacheck", -- Lua
+		"markdownlint", -- Markdown
+		"ruff", -- Python
+		"shellcheck", -- Shell
+		"sqlfluff", -- SQL
+		"yamllint", -- YAML
 
-		-- ─────────────────────────< Python >─────────────────────
-		-- "black", -- Formateador
-		-- "isort", -- Ordena imports
-		-- "pylint", -- Linter
-		"ruff",
+		-- ───────────────────────< FORMATTERS >────────────────────
+		"beautysh", -- Shell
+		"deno", -- JSON / TOML / Markdown
+		"fixjson", -- JSON
+		"prettierd", -- CSS / GraphQL / HTML / JSON / JS / TS / YAML
+		"shfmt", -- Shell
+		"sql-formatter", -- SQL
+		"stylua", -- Lua
+		"taplo", -- TOML
+		"xmlformatter", -- XML
+		"yamlfix", -- YAML
 
-		-- ──────────────────────────< JSON >──────────────────────
-		"deno", -- Formateador: JSON, JSONC y MARKDOWN
-		"fixjson", -- Formateador para JSON5
-		"jsonlint", -- Linter para JSON
-
-		-- ─────────────────────< Multipropósito >─────────────────
-		"prettierd", -- Formateador: CSS, GRAPHQL, HTML,
-
-		-- Lua
-		"luacheck", -- Linter
-		"stylua", -- Formateador
-		"xmlformatter",
-		-- ────────────────────────< Markdown >────────────────────
-		"markdownlint", -- Linter
-
-		-- ───────────────────────────< SQL >──────────────────────
-		"sql-formatter", -- Formateador
-		"sqlfluff", -- Linter
-
-		-- ──────────────────────────< TOML >──────────────────────
-		"taplo", -- Formateador
-
-		-- ──────────────────────────< YAML >──────────────────────
-		"yamlfix", -- Formateador que conserva comentarios
-		"yamllint", -- Linter
+		-- ───────────────────────────< LSP >───────────────────────────
+		-- (configurado en mason-lspconfig.lua)
 	},
-	run_on_start = true,
+
 	auto_update = true,
+	debounce_hours = 12,
+	run_on_start = true,
+	start_delay = 2000,
+
+	integrations = {
+		["mason-lspconfig"] = true,
+		["mason-null-ls"] = false,
+		["mason-nvim-dap"] = false,
+	},
 })
